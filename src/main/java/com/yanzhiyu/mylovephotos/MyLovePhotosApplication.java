991090e9a47278630e193cefea99c672e9362e0f
zhiyuyim <zhiyu_yim@163.com>
2025-09-19 23:45:27 +0800

    first commit

---
+++ src/main/java/com/yanzhiyu/mylovephotos/MyLovePhotosApplication.java
@@ -0,0 +1,13 @@
+package com.yanzhiyu.mylovephotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLovePhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLovePhotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanzhiyu/mylovephotos/MyLovePhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanzhiyu.mylovephotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLovePhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
